6b22c461d9c179bd14db56f11156cec5759846e1
Dieter Raber <dieter@taotesting.com>
2015-03-06 14:50:00 +0100

    typo

--- views/js/controller/cssSdk/cssSdk.js
+++ views/js/controller/cssSdk/cssSdk.js
@@ -59,7 +59,7 @@ define([
             }
 
             feedback()[interactionHook.type](reportTpl(interactionHook),
-                {timeout: {  info: 6000, success: 66000, warning: 6000, error: 6000}});
+                {timeout: {  info: 6000, success: 6000, warning: 6000, error: 6000}});
         });
 
         cssContainer.on('fail.uploader', function (e, file, interactionHook) {
